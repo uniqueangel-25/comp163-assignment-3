c8da485f62212af08111604bd236af3c8d8a2310
Angel Drake <adrake1@aggies.ncat.edu>
2025-09-25 00:48:13 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ uniqueangel-25_assignment_3.py
@@ -0,0 +1,12 @@
+student_name = "Angel"
+current_gpa = 3.4
+study_hours = 20
+social_points = 50
+stress_level = 80
+
+print("Welcome! Your name is",student_name)
+print("Your stats are as followed:")
+print("Your GPA is:",current_gpa)
+print("Your study hours:",study_hours)
+print("Your social points:",social_points)
+print("Your stress level:",stress_level, "\n")
